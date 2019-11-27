980ba6f3f8bda72fbacef4a9d7a764ee90db5e85
tuxubaev17 <tuxubaev17@mail.com>
2019-11-27 19:56:24 +0600

    Create test final project

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.is_element_present(*MainPageLocators.LOGIN_LINK)
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
         # alert = self.browser.switch_to.alert
         # alert.accept()
