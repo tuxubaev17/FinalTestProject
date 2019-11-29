b56d15c84a2581f61a7bc77e4c7f170a3c91eba3
tuxubaev17 <tuxubaev17@mail.com>
2019-11-29 16:46:33 +0600

    Small improvements

--- conftest.py
+++ conftest.py
@@ -1,9 +1,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-
-chromedriver = '/Users/alikhantuxubayev/Documents/environments/chromedriver'
 
 
 def pytest_addoption(parser):
@@ -15,7 +12,7 @@ def browser(request):
     language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
-    browser = webdriver.Chrome(chromedriver, options=options)
+    browser = webdriver.Chrome(options=options)
     browser.check = language
     yield browser
     print("\nquit browser..")
